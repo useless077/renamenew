f83083550a1c984c456bc2a46de97f07e9417f6f
useless077 <95554539+useless077@users.noreply.github.com>
2022-03-02 22:59:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,3 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/useless077/renamer.git /renamer
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /renamer
-fi
-cd /rename 
-pip3 install -U -r requirements.txt
-echo "Starting Bot...."
-python3 bot.py
+git clone https://github.com/useless077/renamer
+cd renamer
+python3 -m renamer
